088437410c08a30568ef4643bffcf7f7fa2bd8a0
MudCitizen <mudcitizen@gmail.com>
2017-04-25 17:58:24 -0400

    After Chapter 7 / Displaying a List of Products

--- SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -4,6 +4,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Moq;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
 
 namespace SportsStore.WebUI.Infrastructure
 {
@@ -29,6 +32,13 @@ namespace SportsStore.WebUI.Infrastructure
 
         protected void AddBindings()
         {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new List<Product> {
+                new Product { Name = "Football", Price = 25 },
+                new Product { Name = "Surf board", Price = 179 },
+                new Product { Name = "Running shoes", Price = 95 }
+                });
+			kernel.Bind<IProductRepository>().ToConstant(mock.Object);
         }
     }
 }
