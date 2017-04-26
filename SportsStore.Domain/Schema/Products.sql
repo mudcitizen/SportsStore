79a156989c9b3f4c7175ddd4e32ac95aebfa7feb
MudCitizen <mudcitizen@gmail.com>
2017-04-25 20:06:09 -0400

    After Chapter 7 / Prepare DB / Add Data

---
+++ SportsStore.Domain/Schema/Products.sql
@@ -0,0 +1,19 @@
+USE [C:\USERS\LANEK\GOOGLE DRIVE\ASP.NET.MVC\SPORTSSTORE\SPORTSSTORE.DOMAIN\APP_DATA\SPORTSSTORE.MDF]
+GO
+
+/****** Object: Table [dbo].[Products] Script Date: 4/25/2017 7:34:56 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Products] (
+    [ProductID]   INT             IDENTITY (1, 1) NOT NULL,
+    [Name]        NVARCHAR (100)  NOT NULL,
+    [Description] NVARCHAR (500)  NOT NULL,
+    [Category]    NVARCHAR (50)   NOT NULL,
+    [Price]       DECIMAL (16, 2) NOT NULL
+);
+
+
